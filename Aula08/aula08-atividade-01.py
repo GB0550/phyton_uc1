38b5473e2b4404274c5ef7644c79d622774edd6f
GB0550 <gabriel.cruz.barcelos@gmail.com>
2025-08-12 20:56:30 -0300

    aula08-atividade01-01.py

---
+++ Aula08/aula08-atividade-01.py
@@ -0,0 +1,21 @@
+#exercicios aula 8 python
+#data 12/08/2025
+"""
+Função Saudação
+     - Imprime uma mensagem de boas vindas
+"""
+def saudacao() :
+    print(f"\n\n\tOlá usuário, seja bem vindo !!\n\n")
+
+
+##
+# Corpo do Programa
+##
+saudacao()
+
+
+
+def soma(a, b):
+  return a + b
+resultado = soma(2, 3)
+print("a soma é", resultado)

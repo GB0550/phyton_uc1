0b2454af18ccb6cd03efbbd41abc6dfa2479f919
GB0550 <gabriel.cruz.barcelos@gmail.com>
2025-08-12 21:49:30 -0300

    aula08-atividade02.py

---
+++ aula08-atividade02.py
@@ -0,0 +1,48 @@
+#aula 08 - atividade 02 
+#Nome: Gabriel
+#data: 12/08/25
+
+"""
+Modifique o seguinte código para usar a função main() corretamente:
+"""
+#def main():
+   # print("\n\n\tOlá mundo!\n\n")
+
+#if __name__ == "__main__":
+  # main()
+
+"""
+def main():
+    n1 = float(input("Digite o primeiro número: "))
+    n2 = float(input("Digite o segundo número: "))
+    soma = n1 + n2
+    print(f"A soma é: {soma}")
+
+main()  """ 
+
+#exercicio 3 
+"""
+def soma(a, b):
+   return a + b
+
+def subtracao(a, b):
+   return a - b
+
+def main():
+   resultado_soma = soma(10, 5)
+   resultado_subtracao = subtracao(10, 5)
+   print(f"Soma: {resultado_soma}")
+   print(f"Subtração: {resultado_subtracao}")
+
+if __name__ == "__main__":
+   main()  """
+
+   #exercicio 4
+
+def main():
+   for i in range(1, 11):
+       print(i)
+
+if __name__ == "__main__":
+   main()
+   

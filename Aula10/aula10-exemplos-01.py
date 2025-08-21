9860b2fdb39c1382aeab587a2445abf5c9649e5d
GB0550 <gabriel.cruz.barcelos@gmail.com>
2025-08-21 20:20:46 -0300

    text

--- Aula10/aula10-exemplos-01.py
+++ Aula10/aula10-exemplos-01.py
@@ -59,8 +59,14 @@ for linha in matriz_2 :
 #codigo 6
 for linha in matriz_2 :
     for valor in linha :
-        print(f"{valor}", end=" ")
-#codigo 
+        print(f"{valor:02}", end=" ")
+
+# neste código utilizamos o "for" de maneira similar a outras linguagens de programação.
+for i in range(3): 
+    for j in range(3):
+        print(f"Elemento ({i},{j}) >>> {matriz_2 [i][j]:02}")
+
+#exercicios 4,5,6
 """   
 matriz_2 = []
 for i in range(3): #linha
@@ -73,3 +79,18 @@ for i in range(3): #linha
 for linha in matriz_2:
     print(linha) 
 """
+#4
+matriz=[]
+for i in range(4):
+    linha=[]
+    for j in range(4):
+        valor=float(input("Digite um valor entre 0 e 99: "))
+        linha.append(valor)
+    matriz.append(linha)
+print(f"{matriz}")
+for linha in matriz: 
+    maior = 0
+    for valor in linha:
+        if valor > maior:
+            maior = valor
+    print(f"O maior valor da linha: {linha} \n\t\t >> {maior}<<")

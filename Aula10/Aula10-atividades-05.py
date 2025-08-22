a935d6bd3371ec85850fc67b4876c16906346221
GB0550 <gabriel.cruz.barcelos@gmail.com>
2025-08-21 21:02:17 -0300

    text

---
+++ Aula10/Aula10-atividades-05.py
@@ -0,0 +1,14 @@
+matriz=[]
+for i in range(4):
+    linha=[]
+    for j in range(4):
+        valor=float(input("Digite um valor entre 0 e 99: "))
+        linha.append(valor)
+    matriz.append(linha)
+print(f"{matriz}")
+par = 0
+for i in range(4): 
+    for j in range(4):
+        if matriz[i][j] % 2 == 0:
+         par += 1
+print(f"Quantidade de números pares: {par}")

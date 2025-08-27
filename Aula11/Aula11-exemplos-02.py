5340ae9acfb5cf807579580043588f2906cfdc81
GB0550 <gabriel.cruz.barcelos@gmail.com>
2025-08-26 21:40:12 -0300

    t

---
+++ Aula11/Aula11-exemplos-02.py
@@ -0,0 +1,11 @@
+alunos = {}
+
+alunos[1] = {"nome": "Maria", "notas": [7.5, 8.0, 9.2]}
+alunos[2] = {"nome": "João", "notas": [6.0, 7.8, 8.5]}
+alunos[3] = {"nome": "Carlos", "notas": [5.5, 6.5, 7.0]}
+for id_aluno, info in alunos.items():
+    notas = info["notas"]
+    media = sum(notas) / len(notas)
+    info["média"] = round(media, 2)
+
+print(alunos)
